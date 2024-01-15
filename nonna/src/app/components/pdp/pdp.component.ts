d9e2705490905983df6a308cb2253918ab672473
Emanuele Maggino <emanuele.maggino@liparipeople.com>
2024-01-15 16:41:25 +0100

    added the possibility to go back in pages

--- nonna/src/app/components/pdp/pdp.component.ts
+++ nonna/src/app/components/pdp/pdp.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pdp',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, RouterModule],
   templateUrl: './pdp.component.html',
   styleUrl: './pdp.component.css'
 })
