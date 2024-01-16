53e1bec14742708548847a24da4b8cb6d3c21e8c
Emanuele Maggino <emanuele.maggino@liparipeople.com>
2024-01-16 12:16:19 +0100

    login second validation

--- nonna/src/app/components/login/login.component.ts
+++ nonna/src/app/components/login/login.component.ts
@@ -1,24 +1,43 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { RouterModule } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, RouterModule, MatButtonModule],
+  imports: [MatFormFieldModule, RouterModule, MatButtonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  userValue = ''
-  passValue = ''
+  userValue: any;
+  passValue: any;
 
-  checkIfEmpty() {
-    if(this.userValue === '' || this.passValue === '') {
-      console.log('not');
+  inputValue: string = '';
+
+  // inputValue() {
+  //   this.userValue = document.querySelector('input')
+
+  //   console.log(this.userValue);
+
+  // }
+
+  user = {
+    isLogged: false,
+  };
+
+  checkInput() {
+    if (this.userValue === '' || this.passValue === '') {
+      return alert('inserisci qualcosa');
+    } else if (this.userValue === 'admin' || this.passValue === 'admin') {
+      this.user.isLogged = true;
+    } else {
+      return alert('credenziali non corrette')
     }
+    console.log(this.user);
   }
+
+  
 }
