a8a604e02770363ece110c237769a9d0226e8823
Emanuele Maggino <emanuele.maggino@liparipeople.com>
2024-01-17 11:28:13 +0100

    list update

--- nonna/src/app/components/tipi/tipi.component.ts
+++ nonna/src/app/components/tipi/tipi.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { ListService } from '../../services/list.service';
+import { NgFor, NgIf } from '@angular/common';
+import { LoginService } from '../../services/login.service';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-tipi',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    RouterModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatPaginatorModule,
+    MatTableModule,
+    NgFor,
+    NgIf,
+  ],
   templateUrl: './tipi.component.html',
-  styleUrl: './tipi.component.css'
+  styleUrl: './tipi.component.css',
 })
 export class TipiComponent {
+  constructor(
+    private listService: ListService,
+    private loginService: LoginService
+  ) {}
 
+  totalItems = this.listService.totalItems;
+  pageSize = this.listService.pageSize;
+
+  onPageChange(event: PageEvent) {
+    this.listService.pageChanged.emit(event);
+
+    const startIndex = event.pageIndex * event.pageSize;
+    const endIndex = startIndex + event.pageSize;
+    const currentPageData = this.tipoPiatto.slice(startIndex, endIndex);
+  }
+
+  mockPrimi = this.listService.mockPrimi
+  mockSecondi = this.listService.mockDolci
+  mockDolci = this.listService.mockSecondi
+  mockFrutta = this.listService.mockFrutta
+
+  value: string = '';
+  isLogged = this.loginService.isLogged;
+
+  tipoPiatto = this.listService.tipoPortata;
+
+  element = document.getElementById('ozi');
+
+  // delete() {
+  //   for () {
+  //     if (this.mockCard.length > 1) {
+  //       this.mockCard.splice(card.id);
+  //     } else {
+  //       alert('non puoi rimuovere questo elemento, aggiungine prima uno nuovo!');
+  //     }
+  //   }
+  //   console.log('item removed');
+  // }
 }
