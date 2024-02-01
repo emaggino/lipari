254ff499b34c2e43a65cd72e596e3ca4a58725f1
Emanuele Maggino <emanuele.maggino@liparipeople.com>
2024-02-01 10:54:08 +0100

    interceptor + fix

--- nonna/src/app/components/add/add.component.ts
+++ nonna/src/app/components/add/add.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [MatFormFieldModule, RouterModule, FormsModule],
+  imports: [MatFormFieldModule, MatButtonModule, RouterModule, FormsModule],
   templateUrl: './add.component.html',
   styleUrl: './add.component.css',
 })
--- nonna/src/app/generic.interceptor.ts
+++ nonna/src/app/generic.interceptor.ts
@@ -1,12 +1,24 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export class GenericInterceptor implements HttpInterceptor {
   constructor() {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      const localToken = localStorage.getItem('accessToken');
-      req = req.clone({headers: req.headers.set('Authorization', 'bearer ' + localToken)})
-      return next.handle(req)
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    debugger;
+    const localToken = localStorage.getItem('accessToken');
+    req = req.clone({
+      headers: req.headers.set('Authorization', 'bearer ' + localToken),
+    });
+    return next.handle(req);
   }
 }
